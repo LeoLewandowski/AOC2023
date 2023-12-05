28ebe04877826f3141e0c0639cb61fbd81196501
LeoLewandowski <leo.lewandowski@student.junia.com>
2023-12-06 00:05:16 +0100

    Day 5 (only part 1 :( )

--- calendar/5.js
+++ calendar/5.js
@@ -1,9 +1,9 @@
 module.exports = {
     desc: 'Seeds',
     execute(string) {
-        const lists = string.split(/.*-to-.*/g).slice(1).map(l => l.split('\r\n').filter(x => x).map(c => c.split(' ').filter(x => x).map(c => parseInt(c))).sort((a,b) => b[1] - a[1]));
+        const lists = string.split(/.*-to-.*/g).slice(1).map(l => l.split('\r\n').filter(x => x).map(c => c.split(' ').filter(x => x).map(c => parseInt(c))).sort((a, b) => b[1] - a[1]));
         var seeds = string.match(/(?<=seeds: ).+/)[0].split(' ').filter(x => x).map(y => parseInt(y));
-        var seedRanges = string.match(/(?<=seeds: ).+/)[0].match(/[0-9]+ [0-9]+/g).map(r => r.split(' ').map(x => parseInt(x)));
+        var ranges = string.match(/(?<=seeds: ).+/)[0].match(/[0-9]+ [0-9]+/g).map(r => r.split(' ').map(x => parseInt(x)));
 
         // string.match(/(?<=seeds: ).+/)[0].match(/[0-9]+ [0-9]+/g).forEach(r => {
         //     var range = r.split(' ').map(x => parseInt(x));
@@ -12,51 +12,59 @@ module.exports = {
         //     seedRanges.push(x);
         // });
 
-        console.log(seedRanges);
+        for (let list of lists) {
+            var nextSeeds = [...seeds], nextRanges = [];
 
-        for(let list of lists) {
-            var nextSeeds = [...seeds], nextRanges = [...seedRanges];
-
-            for(let j = 0; j < seeds.length; j++){
-                for(let line of list) {
+            for (let j = 0; j < seeds.length; j++) {
+                for (let line of list) {
                     let diff = seeds[j] - line[1];
 
-                    if(diff >= 0 && diff < line[2]){
+                    if (diff >= 0 && diff < line[2]) {
                         nextSeeds[j] = diff + line[0];
                         break;
                     }
                 }
             }
 
-            // for(let k = 0; k < seedRanges.length; k++){
-            //     for(let l = 0; l < seedRanges[k].length; l++){
-            //         for(let line of list) {
-            //             let diff = seedRanges[k][l] - line[1];
-
-            //             if(diff >= 0 && diff < line[2]){
-            //                 nextRanges[k][l] = diff + line[0];
-            //                 break;
-            //             }
-            //         }
-            //     }
-            // }
-            console.log('--------------------------------------------------------------------');
-            for(let k = 0; k < seedRanges.length; k++){
-                let range = seedRanges[k];
-                console.log(range, k)
-                for(let l = 0; l < range[1]; l++){
-                    console.log(l, '.................');
-                    for(let line of list){
-                        let diff = range[0] + l - line[1];
-                        console.log(diff);
-                    }
+            for(let i = 0; i < ranges.length; i++) {
+                var nextRange = [ranges[i]];
+                for (let line of list) {
+                    var res = overlappingRanges(ranges[i], line);
+                    if(res.length == 1 && res[0][0] == ranges[i][0]) continue;       
+                    nextRange = res;
+                    break;
                 }
+                nextRanges = nextRanges.concat(nextRange);
             }
-
             seeds = nextSeeds;
-            seedRanges = nextRanges;
+            ranges = nextRanges;
         }
 
-        return [Math.min(...seeds), NaN/*Math.min(...seedRanges.map(x => Math.min(...x)))*/];
+        var result2 = ranges.reduce((a, c) => Math.min(a, c[0]), Infinity);
+
+        return [Math.min(...seeds), result2/*Math.min(...seedRanges.map(x => Math.min(...x)))*/];
     }
+}
+
+/**
+ * 
+ * @param {number[]} range 
+ * @param {number[]} line 
+ */
+function overlappingRanges(range, line) {
+    let before = [...range], overlap = [-1, 0];
+    for (let i = 0; i < range[1]; i++) {
+        // Overlap
+        if (i + range[0] >= line[1] && i + range[0] < line[2] + line[1]) {
+            if (overlap[0] == -1) {
+                before[1] = i;
+                overlap[0] = line[0] + Math.max(range[0] - line[1], 0);
+            }
+            overlap[1]++;
+        }
+    }
+    // Values after the overlap
+    let after = [overlap[0] + overlap[1], range[1] - overlap[1] - before[1]];
+
+    return [before, overlap, after].filter(r => r && r[0] >= 0 && r[1] > 0);
 }
---
+++ calendar/test.js
@@ -0,0 +1,51 @@
+const { log } = require("console");
+
+module.exports = {
+    desc: 'Test file',
+    execute(string) {
+        const lists = string.split(/.*-to-.*/g).slice(1).map(l => l.split('\r\n').filter(x => x).map(c => c.split(' ').filter(x => x).map(c => parseInt(c))).sort((a, b) => b[1] - a[1]));
+        var ranges = string.match(/(?<=seeds: ).+/)[0].match(/[0-9]+ [0-9]+/g).map(r => r.split(' ').map(x => parseInt(x)));
+        console.log(ranges);
+
+        for (let list of lists) {
+            for (let line of list) {
+                console.log('---------------------------------------\nline : ',line);
+                for (let i = 0; i < ranges.length; i++) {
+                    var res = overlappingRanges(ranges[i], line);
+                    console.log(ranges[i], ' --> ', res, '\n');
+                    ranges[i] = res.shift();
+                    ranges = ranges.concat(res);
+                }
+                console.log(ranges);
+            }
+        }
+
+        console.log(ranges);
+        
+        return [NaN, NaN];
+    }
+}
+
+/**
+ * 
+ * @param {number[]} range 
+ * @param {number[]} line 
+ */
+function overlappingRanges(range, line) {
+    let before = [...range], overlap = [-1, 0];
+    for (let i = 0; i < range[1]; i++) {
+        // Overlap
+        if (i + range[0] >= line[1] && i + range[0] < line[2] + line[1]) {
+            if (overlap[0] == -1) {
+                before[1] = i;
+                overlap[0] = line[0] + (range[0] - line[1]);
+                console.log(line[0] + (range[0] - line[1]));
+            }
+            overlap[1]++;
+        }
+    }
+    // Values after the overlap
+    let after = [overlap[0] + overlap[1], range[1] - overlap[1] - before[1]];
+
+    return [before, overlap, after].filter(r => r && r[0] >= 0 && r[1] > 0);
+}
