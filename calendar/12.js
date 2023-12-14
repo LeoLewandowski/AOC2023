3c5c1e902d3eeca045c4a4c9da38441060f57d0b
LeoLewandowski <leo.lewandowski@student.junia.com>
2023-12-14 22:04:14 +0100

    Day 14 !!!!! Works !!!!!!

--- calendar/12.js
+++ calendar/12.js
@@ -1,23 +1,39 @@
 module.exports = {
-    desc:'Hot springs',
+    desc: 'Hot springs',
     execute(input) {
         let lengths = [], springs = [];
         input.split('\r\n').forEach(line => {
+            console.log('----------------------------- Line ---------------------------\n', line)
             let lengthsLine = line.split(' ');
             let springsLine = lengthsLine.shift();
             lengthsLine = lengthsLine[0].split(',').map(n => parseInt(n));
-            isValidRow(springsLine,lengthsLine);
+
+            let possibleSpaces = Array.from(springsLine.matchAll(/\?/g)).map(x => x.index);
+            let springsToPlace = lengthsLine.reduce((a,c) => a += c) - (springsLine.match(/#/g)?.length);
+
+            placeSpring(springsLine, lengthsLine, possibleSpaces, springsToPlace);
+            console.log(totalPart1);
         });
-        return [NaN,NaN];
+        return [totalPart1, NaN];
     }
 }
 
-function isValidRow(springs,lengths){
-    let emptySpaces = Array.from(springs.matchAll(/\?/g));
-    let springsToPlace = lengths.reduce((a,c) => a+c) - springs.match(/#/g).length;
-    console.log(springs, lengths, springsToPlace, emptySpaces);
+let totalPart1 = 0;
+
+function isValidRow(springs, lengths) {
+    let match = springs.match(/[#]+/g);
+    if (lengths.length != match.length) return false;
+    for (let i = 0; i < lengths.length; i++) {
+        if (match[i].length != lengths[i]) return false;
+    }
+    return totalPart1++;
 }
 
-function placeSpring(springs,lengths,springsToPlace){
-
+function placeSpring(springs, lengths, possibleSpaces, springsToPlace) {
+    if(springsToPlace == 0) return isValidRow(springs, lengths);
+    while(possibleSpaces.length && possibleSpaces.length >= springsToPlace){
+        let space = possibleSpaces.shift();
+        // console.log(springs.slice(0,space) + '#' + springs.slice(space + 1), possibleSpaces, springsToPlace - 1);
+        placeSpring(springs.slice(0,space) + '#' + springs.slice(space + 1), lengths, [...possibleSpaces], springsToPlace - 1);
+    }
 }
---
+++ calendar/14.js
@@ -0,0 +1,105 @@
+const { exec } = require("child_process");
+
+module.exports = {
+    desc: 'Mirror',
+    execute(input) {
+        platformSpin = input.split('\r\n');
+        let platform = [...platformSpin];
+        rollRocksNorth(platform);
+
+        let i = 0;
+        for (i; i < IT_NB; i++) {
+            let str = platformSpin.join('');
+            if (Cache.get(str)) {
+                if (str == LoopPlatform) break;
+                if (!LoopSize) LoopPlatform = str;
+                LoopSize++;
+            }
+            rollRocksNorth();
+            rollRocksWest();
+            rollRocksSouth();
+            rollRocksEast();
+            Cache.set(str, true);
+        }
+        for (let j = 0; j < (IT_NB - i) % LoopSize; j++) {
+            rollRocksNorth();
+            rollRocksWest();
+            rollRocksSouth();
+            rollRocksEast();
+         }
+        return [countLoad(platform), countLoad(platformSpin)];
+    }
+}
+
+var platformSpin
+
+const IT_NB = 1_000_000_000;
+const Cache = new Map();
+let LoopSize = 0, LoopPlatform;
+
+function rollRocksNorth(p = platformSpin) {
+    for (let y = 0; y < p.length; y++) {
+        for (let x = 0; x < p[0].length; x++) {
+            if (p[y][x] != 'O') continue;
+            for (let i = 0; i < y; i++) {
+                if ('#O'.includes(p[y - i - 1][x])) break;
+                p[y - i] = p[y - i].slice(0, x) + '.' + p[y - i].slice(x + 1);
+                p[y - i - 1] = p[y - i - 1].slice(0, x) + 'O' + p[y - i - 1].slice(x + 1);
+            }
+        }
+    }
+    return p;
+}
+
+function rollRocksWest() {
+    for (let y = 0; y < platformSpin.length; y++) {
+        for (let x = 0; x < platformSpin[0].length; x++) {
+            if (platformSpin[y][x] != 'O') continue;
+            for (let i = 0; i < x; i++) {
+                if ('#O'.includes(platformSpin[y][x - i - 1])) break;
+                platformSpin[y] = platformSpin[y].slice(0, x - i - 1) + 'O.' + platformSpin[y].slice(x - i + 1);
+            }
+        }
+    }
+    return platformSpin;
+}
+
+function rollRocksEast() {
+    for (let y = 0; y < platformSpin.length; y++) {
+        for (let x = platformSpin[0].length; x > 0; x--) {
+            if (platformSpin[y][x - 1] != 'O') continue;
+            for (let i = 0; i < platformSpin[0].length - x; i++) {
+                if ('#O'.includes(platformSpin[y][x + i])) break;
+                platformSpin[y] = platformSpin[y].slice(0, x + i - 1) + '.O' + platformSpin[y].slice(x + i + 1);
+            }
+        }
+    }
+    return platformSpin;
+}
+
+function rollRocksSouth() {
+    for (let y = platformSpin.length; y > 0; y--) {
+        for (let x = 0; x < platformSpin[0].length; x++) {
+            if (platformSpin[y - 1][x] != 'O') continue;
+            for (let i = 0; i < platformSpin[0].length - y; i++) {
+                if ('#O'.includes(platformSpin[y + i][x])) break;
+                platformSpin[y + i - 1] = platformSpin[y + i - 1].slice(0, x) + '.' + platformSpin[y + i - 1].slice(x + 1);
+                platformSpin[y + i] = platformSpin[y + i].slice(0, x) + 'O' + platformSpin[y + i].slice(x + 1);
+            }
+        }
+    }
+    return platformSpin;
+}
+
+function countLoad(platform) {
+    let counter = 0;
+    for (let y = 0; y < platform.length; y++) {
+        let m = platform[y].match(/O/g);
+        if (m && m.length > 0) counter += m.length * (platform.length - y);
+    }
+    return counter;
+}
+
+function getDir(i) {
+    return (i > 1 ? (i == 2 ? 'South' : 'East') : (i == 0 ? 'North' : 'West'));
+}
